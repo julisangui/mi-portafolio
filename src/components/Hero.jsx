b1f4cb59ad9b89f7f0ed6c4b2d38111f4f2e3cc5
Julian <juliangel2003@gmail.com>
2025-02-22 02:34:56 -0300

    cambios añadidos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
             <h3 className="cargo">Desarrollador de software</h3>
             <p className="presentacion">
               ¡Hola! soy Julian, tengo 21 años y soy un desarrollador de software con mayor experiencia en el desarrollo front-end.
-              Llevo 2 años creando proyectos personales y colaborativos, que incluyen: desarrollo de aplicaciones web y moviles.
-              Actualmente, continuo perfeccionándome en nuevas tecnologías y aprendiendo nuevas habilidades como el desarrollo de IA
+              Llevo 2 años creando proyectos personales y colaborativos, que incluyen: desarrollo de aplicaciones web y móviles.
+              Actualmente, continúo perfeccionándome en nuevas tecnologías y aprendiendo nuevas habilidades como el desarrollo de IA
               y Machine Learning, mientras que a la vez busco mi primera oportunidad profesional y seguir creciendo como desarrollador.
             </p>
             <a href="/mi-portafolio/Curriculo.pdf" className="boton-cv" download="Curriculo.pdf">Descargar CV<i className="bi bi-download"></i></a>
