b5fb9b98022b3a883380ab8155ee52d9f2facc96
Julian <juliangel2003@gmail.com>
2025-03-05 21:49:29 -0300

    actualizaciones

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,5 +1,4 @@
 import tropicalBeach from '../assets/tropical-beach.jpg';
-import buenosAires from '../assets/buenos-aires.jpg';
 import eCommerce from '../assets/e-commerce.avif';
 
 
@@ -29,29 +28,6 @@ const Proyectos = () => {
         </div>
 
         <div className="tarjeta-proyecto">
-          <img src={buenosAires} className="imagen-proyecto" />
-          <div className="info-proyecto">
-            <h3 className="titulo-proyecto">Página de actividades de la CABA</h3>
-            <p className="descripcion-proyecto">
-              Esta página funciona como una guía turística para todo lo que puedes hacer en la ciudad de Buenos Aires, ver su información y sus actividades.
-            </p>
-            <div className="herramientas-utilizadas">
-              <span>HTML</span>
-              <span>CSS</span>
-              <span>JavaScript</span>
-              <span>React</span>
-              <span>Python</span>
-              <span>MySQL</span>
-            </div>
-            <div className="boton-proyecto">
-              <a href="#" target="_blank" rel="noopener noreferrer" className="btn-enlace">
-                Ir a la página <i className="bi bi-box-arrow-up-right"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div className="tarjeta-proyecto">
           <img src={eCommerce} className="imagen-proyecto" />
           <div className="info-proyecto">
             <h3 className="titulo-proyecto">e-commerce gamer</h3>
@@ -59,7 +35,8 @@ const Proyectos = () => {
               Un e-commerce que ofrece una amplia gama de productos gamer. El sitio incluye la opción de realizar compras y funcionalidades de filtrado y búsqueda para facilitar la navegación.
             </p>
             <div className="herramientas-utilizadas">
-              <span>WordPress</span>
+              <span>Angular</span>
+              <span>TypeScript</span>
               <span>PHP</span>
               <span>MySQL</span>
             </div>
